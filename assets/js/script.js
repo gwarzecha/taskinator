9e9b1b6744344a2ca2d286837c930cc36c71dd5d
gwarz <gmwarzecha@tutanota.com>
2021-01-05 23:04:01 -0700

    function refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 var formEl = document.querySelector("#task-form"); //DOM object reference to the button
 var tasksToDoEl = document.querySelector("#tasks-to-do"); //DOM object reference to the task list
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
   event.preventDefault(); //instructs the browser to not carry out the default broswer behavior
   var taskNameInput = document.querySelector("input[name='task-name']").value; //square brackets select an HTML element by an attribute
   //.value selects JUST the value property from the elment
   var taskTypeInput = document.querySelector("select[name='task-type']").value; 
-  
+
+  // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  }; 
+
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
   // create list item 
   var listItemEl = document.createElement("li"); //a var is created to house the li item
   listItemEl.className = "task-item"; //list item is assigned to the class task-item for stylistic rendering
@@ -16,15 +27,14 @@ var createTaskHandler = function(event) {
   // give it a class name
   taskInfoEl.className = "task-info";
   // add HTML content to div
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl); // adds innerHTML assigned to taskInfoEl to listItemEl
 
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
-  
-}
+};
 
-  formEl.addEventListener("submit", createTaskHandler); //when buttonEl is clicked..
+  formEl.addEventListener("submit", taskFormHandler); //when buttonEl is clicked..
   
 
 
