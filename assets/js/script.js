38a3bc93a76c8dc6f20a89d1db233701bb37faa8
gwarz <gmwarzecha@tutanota.com>
2021-01-05 21:09:09 -0700

    restructured listener to submit and preventDefault

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-var buttonEl = document.querySelector("#save-task"); //DOM object reference to the button
+var formEl = document.querySelector("#task-form"); //DOM object reference to the button
 var tasksToDoEl = document.querySelector("#tasks-to-do"); //DOM object reference to the task list
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+  event.preventDefault(); //instructs the browser to not carry out the default broswer behavior
+
   var listItemEl = document.createElement("li"); //a var is created to house the li item
   listItemEl.className = "task-item"; //list item is assigned to the class task-item for stylistic rendering
   listItemEl.textContent = "This is a new task."; //this is the text that will be assigned to the listItemEl
   tasksToDoEl.appendChild(listItemEl);// this adds the li item to the DOM referenced task list declared above
 }
 
-buttonEl.addEventListener("click", createTaskHandler); //when buttonEl is clicked..
+  formEl.addEventListener("submit", createTaskHandler); //when buttonEl is clicked..
   
 
 
